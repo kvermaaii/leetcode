3a15284accc8aa02cab27e81941b1f053bd017d8
kshitij <138381237+kvermaaii@users.noreply.github.com>
2025-03-30 11:28:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.54%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode outputList = new ListNode(-1);
+        ListNode ptr1 = list1;
+        ListNode ptr2 = list2;
+        ListNode ptr = outputList;
+        while(ptr1 != null && ptr2 != null){
+            if(ptr1.val >ptr2.val){
+                ptr.next = ptr2;
+                ptr2 = ptr2.next;
+            } else{
+                ptr.next = ptr1;
+                ptr1 = ptr1.next;
+            }
+            ptr = ptr.next;
+        }
+        if(ptr1 == null){
+            while(ptr2 != null){
+                ptr.next = ptr2;
+                ptr = ptr.next;
+                ptr2 = ptr2.next;
+            }
+        } 
+        if(ptr2 == null){
+            while(ptr1 != null){
+                ptr.next = ptr1;
+                ptr = ptr.next;
+                ptr1 = ptr1.next;
+            }
+        }
+        return outputList.next;
+    }
+}
