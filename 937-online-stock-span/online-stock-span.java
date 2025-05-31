48e0a42bac187769ecb8b6799a7b2a327bd638a1
kshitij <138381237+kvermaaii@users.noreply.github.com>
2025-05-31 22:21:42 +0530

    Time: 27 ms (91.96%) | Memory: 55.7 MB (21.48%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -5,25 +5,12 @@ class StockSpanner {
     }
     
     public int next(int price) {
-        if(stk.isEmpty()){
-            stk.push(new int[]{price,0});
-            return 1;
-        }
-        if(stk.peek()[0]>price){
-            stk.push(new int[]{price,stk.peek()[1]+1});
-            return 1;
-        }
-        int index = stk.peek()[1]+1;
+        int span=1;
         while(!stk.isEmpty() && !(stk.peek()[0]>price)){
-            stk.pop();
+            span += stk.pop()[1];
         }
-        if(stk.isEmpty()){
-            stk.push(new int[]{price,index});
-            return index+1;
-        }
-        int noOfDays = index-stk.peek()[1];
-        stk.push(new int[]{price,index});
-        return noOfDays;
+        stk.push(new int[]{price,span});
+        return span;
     }
 }
 
