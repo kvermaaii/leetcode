c3a7273521eaf66e40ec742efd0fced3a8655105
kshitij <138381237+kvermaaii@users.noreply.github.com>
2025-06-04 21:40:13 +0530

    Time: 44 ms (78.76%) | Memory: 112.7 MB (82.90%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,39 +1,92 @@
 class LRUCache {
     int capacity;
-    Map<Integer,Integer> map;
-    List<Integer> list;
+    int size;
+    Map<Integer, ListNode> map;
+    ListNode head;
+    ListNode tail;
+
     public LRUCache(int capacity) {
-        list = new LinkedList<>();
-        this.capacity = capacity;
         map = new HashMap<>();
+        this.capacity = capacity;
+        size = 0;
     }
-    
+
     public int get(int key) {
-        if(map.containsKey(key)){
-            list.remove((Integer)key);
-            list.addLast(key);
-            return map.get(key);
-        } else{
-            return -1;
+        if (map.containsKey(key)) {
+            ListNode node = map.get(key);
+            if (node != tail) {
+                // Remove node from current position
+                if (node == head) {
+                    head = head.next;
+                    if (head != null) head.prev = null;
+                } else {
+                    node.prev.next = node.next;
+                    if (node.next != null) node.next.prev = node.prev;
+                }
+                tail.next = node;
+                node.prev = tail;
+                node.next = null;
+                tail = node;
+            }
+            return node.val;
         }
+        return -1;
     }
-    
+
     public void put(int key, int value) {
-        if(map.containsKey(key)){
-            list.remove((Integer)key);
+        if (map.containsKey(key)) {
+            ListNode delete = map.get(key);
+            delete.val = value; // Update value
+            if (delete != tail) {
+                // Remove node from current position
+                if (delete == head) {
+                    head = head.next;
+                    if (head != null) head.prev = null;
+                } else {
+                    delete.prev.next = delete.next;
+                    if (delete.next != null) delete.next.prev = delete.prev;
+                }
+                tail.next = delete;
+                delete.prev = tail;
+                delete.next = null;
+                tail = delete;
+            }
+            return;
         }
-        if(list.size()==capacity){
-            int x = list.removeFirst();
-            map.remove(x);
+
+        if (size == capacity) {
+            // Remove head
+            map.remove(head.key);
+            if (head == tail) {
+                head = tail = null; // List becomes empty
+            } else {
+                head = head.next;
+                head.prev = null;
+            }
+            size--;
         }
-        list.addLast(key);
-        map.put(key,value);
+
+        ListNode newNode = new ListNode(key, value);
+        if (tail == null) { // If the list is empty
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        map.put(key, newNode);
+        size++;
     }
 }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
+class ListNode {
+    int key;
+    int val;
+    ListNode next;
+    ListNode prev;
+
+    ListNode(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
