5872b564910a342fcb35c356a8b6a2bb4ddfda02
kshitij <138381237+kvermaaii@users.noreply.github.com>
2025-06-10 12:05:35 +0530

    Time: 2 ms (26.15%) | Memory: 42.5 MB (65.54%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,45 +1,34 @@
 class Solution {
-    public int myAtoi(String input) {
-        int sign = 1;
-        int result = 0;
-        int index = 0;
-        int n = input.length();
+    public int myAtoi(String s) {
+        s = s.trim();
+        char[] chr = s.toCharArray();
+        int length = s.length();
+        if (length == 0) return 0;
 
-        // Discard all spaces from the beginning of the input string.
-        while (index < n && input.charAt(index) == ' ') {
-            index++;
+        boolean neg = false;
+        int num = 0;
+        int k = 0;
+
+        if (chr[0] == '-') {
+            neg = true;
+            k++;
+        } else if (chr[0] == '+') {
+            k++;
         }
 
-        // sign = +1, if it's positive number, otherwise sign = -1.
-        if (index < n && input.charAt(index) == '+') {
-            sign = 1;
-            index++;
-        } else if (index < n && input.charAt(index) == '-') {
-            sign = -1;
-            index++;
-        }
-
-        // Traverse next digits of input and stop if it is not a digit
-        while (index < n && Character.isDigit(input.charAt(index))) {
-            int digit = input.charAt(index) - '0';
-
-            // Check overflow and underflow conditions.
-            if (
-                (result > Integer.MAX_VALUE / 10) ||
-                (result == Integer.MAX_VALUE / 10 &&
-                    digit > Integer.MAX_VALUE % 10)
-            ) {
-                // If integer overflowed return 2^31-1, otherwise if underflowed return -2^31.
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        for (int i = k; i < length; i++) {
+            if (!Character.isDigit(chr[i])) {
+                break;
             }
 
-            // Append current digit to the result.
-            result = 10 * result + digit;
-            index++;
+            int d = chr[i] - '0';
+            if (num > (Integer.MAX_VALUE - d) / 10) {
+                return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            num = num * 10 + d;
         }
 
-        // We have formed a valid number without any overflow/underflow.
-        // Return it after multiplying it with its sign.
-        return sign * result;
+        return neg ? -num : num;
     }
 }
