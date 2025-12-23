867180ce7215ad8a6e25296a90d590decd7cf964
kshitij <138381237+kvermaaii@users.noreply.github.com>
2025-12-23 12:22:13 +0530

    Time: 22 ms (32.87%) | Memory: 116.8 MB (5.09%) - LeetSync

--- 4138-minimum-deletion-cost-to-make-all-characters-equal/minimum-deletion-cost-to-make-all-characters-equal.java
+++ 4138-minimum-deletion-cost-to-make-all-characters-equal/minimum-deletion-cost-to-make-all-characters-equal.java
@@ -1,25 +1,16 @@
 class Solution {
     public long minCost(String s, int[] cost) {
-        Map<Character, List<Integer>> map = new HashMap<>();
+        Map<Character, Long> map = new HashMap<>();
         char[] charArray = s.toCharArray();
         int n = cost.length;
         long sumOfCosts = 0;
         for(int i = 0; i < n; i++){
-            if(!map.containsKey(charArray[i])){
-                map.put(charArray[i], new ArrayList<>());
-                map.get(charArray[i]).add(i);
-            } else{
-                map.get(charArray[i]).add(i);
-            }
+            map.put(charArray[i], map.getOrDefault(charArray[i], 0L) + cost[i]);
             sumOfCosts += cost[i];
         }
         long minCost = Long.MAX_VALUE;
-        for (List<Integer> indexes : map.values()) {
-            long sum = sumOfCosts;
-            for(int x : indexes){
-                sum -= cost[x];
-            }
-            minCost = Math.min(minCost, sum);
+        for (long x : map.values()) {
+            minCost = Math.min(minCost, sumOfCosts - x);
         }
         return minCost;
     }
