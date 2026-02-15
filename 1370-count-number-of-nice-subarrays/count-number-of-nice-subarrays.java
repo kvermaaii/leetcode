62b0a5db9aa1061bd38ac1e929e6746ed8a7be8c
kshitij <138381237+kvermaaii@users.noreply.github.com>
2026-02-15 10:39:03 +0530

    Time: 14 ms (50.50%) | Memory: 59.9 MB (14.26%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,28 +1,24 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
+        return findSubarrays(nums, k) - findSubarrays(nums, k - 1);
+    }
+    private int findSubarrays(int[] nums, int k){
         int begin = 0;
         int end = 0;
         int oddCount = 0;
         int noOfSubarray = 0;
-        int last = -1;
-        Deque<Integer> pq = new ArrayDeque<>();
         while(end < nums.length){
             int curr = nums[end];
             if(curr % 2 == 1){
-                last = end;
-                pq.addLast(end);
                 oddCount++;
             }
             while(oddCount > k){
                 if(nums[begin] % 2 == 1){
                     oddCount--;
-                    pq.removeFirst();
                 }
                 begin++;
             }
-            if(oddCount == k && !pq.isEmpty()){
-                noOfSubarray += pq.peekFirst() - begin + 1;
-            }
+            noOfSubarray += end - begin + 1;
             end++;
         }
         return noOfSubarray;
