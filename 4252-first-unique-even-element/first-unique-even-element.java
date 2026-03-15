8e06a6ea0340d51e6178f75bff300a6f10b63f9e
kshitij <138381237+kvermaaii@users.noreply.github.com>
2026-03-16 00:02:14 +0530

    Time: 2 ms (50.00%) | Memory: 46.2 MB (50.00%) - LeetSync

--- 4252-first-unique-even-element/first-unique-even-element.java
+++ 4252-first-unique-even-element/first-unique-even-element.java
@@ -1,20 +1,15 @@
-
 class Solution {
     public int firstUniqueEven(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        // Count frequency of each number
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(nums[i]%2 == 0) map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-
-        // Find first even number with frequency 1
-        for (int num : nums) {
-            if (num % 2 == 0 && map.get(num) == 1) {
+        for(int num : nums){
+            if(num % 2 == 0 && map.get(num) == 1){
                 return num;
             }
         }
-
         return -1;
     }
 }
