a763c90c6fb794dab5ccf9a019bb754fc068b544
kshitij <138381237+kvermaaii@users.noreply.github.com>
2026-04-11 19:47:33 +0530

    Time: 915 ms (20.73%) | Memory: 186.5 MB (12.68%) - LeetSync

---
+++ 4065-count-sequences-to-k/count-sequences-to-k.java
@@ -0,0 +1,20 @@
+class Solution {
+    HashMap<String,Integer> dp = new HashMap<>();
+    public int countSequences(int[] nums, long k) {
+        dp.clear();
+        return rec(nums,0,k,1,1);
+    }
+    public int rec(int[] arr, int i, long k, long num, long den){
+        if(i >= arr.length){
+            return (num == k*den) ? 1 : 0;
+        }
+        String st = num + "_" + den + "_" + i;
+        if(dp.containsKey(st)) return dp.get(st);
+        int count = 0;
+        count += rec(arr, i+1, k, arr[i]*num, den);
+        count += rec(arr, i+1, k, num, den*arr[i]);
+        count += rec(arr, i+1, k, num, den);
+        dp.put(st,count);
+        return count;
+    }
+}
