54389e090728281d3990e8da58c3b75650c74890
kshitij <138381237+kvermaaii@users.noreply.github.com>
2026-08-28 12:00:30 +0530

    Time: 50 ms (49.86%) | Memory: 47.6 MB (55.96%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,19 +1,24 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            sum += nums[i];
+        int totalSum = 0;
+        for(int num: nums){
+            totalSum += num;
         }
-        Boolean[][] dp = new Boolean[n][sum];
-        return rec(nums, 0, 0, sum, dp);
-    }
-    boolean rec(int[] nums, int i , int sum, int t, Boolean[][] dp){
-        if(t - 2*sum == 0) return true;
-        if(i >= nums.length) return false;
-        if(dp[i][sum] != null) return dp[i][sum];
-        boolean pick = rec(nums, i+1, sum + nums[i],t,dp);
-        boolean pickNot = rec(nums, i+1, sum,t, dp);
-        return dp[i][sum] = pick || pickNot;
+        int n = nums.length;
+        if(totalSum % 2 == 1) return false;
+        totalSum /= 2;
+        boolean[][] dp = new boolean[n+1][totalSum+1]; // i = 0 means we are not picking anything null subset thats why dp size is n+1  and also the current itration will be wit i-1
+        //base case
+        //if totalSum > 0 && subset is null value will bea false only dp[0][0] = true;
+        dp[0][0] = true;
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= totalSum; j++){
+                boolean pick = false;
+                if(j >= nums[i-1]) pick = dp[i-1][j-nums[i-1]];
+                boolean pickNot = dp[i-1][j]; 
+                dp[i][j] = pick || pickNot;
+            }
+        }
+        return dp[n][totalSum];
     }
 }
