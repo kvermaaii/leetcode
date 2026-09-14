40babc326c866343372f7fa98d2bd8929a68149d
kshitij <138381237+kvermaaii@users.noreply.github.com>
2026-09-15 01:50:51 +0530

    Time: 19 ms (99.26%) | Memory: 48.4 MB (93.22%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,23 +1,32 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int n = nums.length;
+        if(n == 0) return 0;
+        if(n == 1) return k == nums[0]? 1:0;
         int[] ps = new int[n];
+        Map<Integer, Integer> map = new HashMap<>();
         ps[0] = nums[0];
         for(int i = 1; i < n; i++){
             ps[i] = ps[i-1] + nums[i];
         }
         int ans = 0;
-        for(int i = 0; i < n ;i++){
-            for(int j = i; j < n; j++){
-                int sum;
-                if(i == 0){
-                    sum = ps[j];
-                }else{
-                    sum = ps[j] - ps[i-1];
-                }
-                if(sum == k) ans++;
-            }
+        for(int i = 0; i < n; i++){
+            if(ps[i] == k) ans++;
+            ans += map.getOrDefault(ps[i] - k,0); 
+            map.put(ps[i],map.getOrDefault(ps[i],0)+1);
         }
+        // int ans = 0;
+        // for(int i = 0; i < n ;i++){
+        //     for(int j = i; j < n; j++){
+        //         int sum;
+        //         if(i == 0){
+        //             sum = ps[j];
+        //         }else{
+        //             sum = ps[j] - ps[i-1];
+        //         }
+        //         if(sum == k) ans++;
+        //     }
+        // }
         return ans;
     }
 }
